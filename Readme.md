96a315a5b562fc2b04d20a7319f4ec17ce26fbde
Christian Ehrig <cehrig@cloudflare.com>
2020-08-19 01:42:04 +0200

    initial import

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# Cloudflare Traffic by Account & Zone
+This script will pull various metrics from Cloudflare's GraphQL API by Account and Zone Name
+- Egress Bytes
+- Cached Egress Bytes
+- Cached Requests
+- Encryptes Bytes
+- Encrypted Requests
+- Page Views
+- Requests
+
+The script will generate a CSV using the fields above, plus
+- Account name
+- Zone name
+
+### Authentication
+Authenticate against the GraphQL API by exporting your Cloudflare Email and global API token
+```
+export CLOUDFLARE_API_KEY=<your API token>
+export CLOUDFLARE_EMAIL=<your Email>
+```
+
+### Run the script
+
+`./traffic.sh 2020-07-01 2020-07-31`
+
+to pull usage metrics from the 1st to the 31st of July 2020
---
+++ traffic.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+set -e
+
+# Trapping SIGTERM
+trap "exit 1" TERM
+
+# Used in functions to stop execution on error
+export SCIPRT_PID=$$
+
+# Exits program with a message sent to stderr
+function exit_with_error {
+  >&2 echo "$1"
+  kill -s TERM $SCIPRT_PID
+}
+
+# Pull accounts by API Key
+function get_accounts {
+  data=$(curl -s -X GET "https://api.cloudflare.com/client/v4/accounts?page=1&per_page=50" \
+    -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
+    -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
+    -H "Content-Type: application/json")
+  success=$(echo "$data" | jq -r '.success')
+
+  if [ "$success" != "true" ]; then
+    exit_with_error "error reading accounts: $(echo "$data" | jq -r '.errors')"
+  fi
+
+  accountTags=$(echo "$data" | jq -r '.result[] | "\(.id)#\(.name)"')
+  echo "$accountTags"
+}
+
+# Pull zones by Account ID
+function get_zones {
+  data=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?&account.id=$1&page=1&per_page=1000" \
+    -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
+    -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
+    -H "Content-Type: application/json")
+  success=$(echo "$data" | jq -r '.success')
+
+  if [ "$success" != "true" ]; then
+    exit_with_error "error reading zones: $(echo "$data" | jq -r '.errors')"
+  fi
+
+  zoneTags=$(echo "$data" | jq -r '.result[] | "\(.id)#\(.name)"')
+  echo "$zoneTags"
+}
+
+# Pull egress bytes per zoneTag
+function get_traffic {
+  data=$(curl -s "https://api.cloudflare.com/client/v4/graphql" \
+    -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
+    -H "X-Auth-Key: $CLOUDFLARE_API_KEY" \
+    --data-binary '{"operationName":"GetHeadlineStats","variables":{"zoneTag":"'"$1"'","limit":10000,"filter":{"date_geq":"'"$2"'","date_leq":"'"$3"'"}},"query":"query GetHeadlineStats {\n  viewer {\n    zones(filter: {zoneTag: $zoneTag}) {\n      total: httpRequests1dGroups(filter: $filter, limit: $limit) {        uniq {\n          uniques\n          __typename\n        }\n        __typename\n      }\n      statsOverTime: httpRequests1dGroups(filter: $filter, limit: $limit) {\n        sum {\n          requests\n          bytes\n          pageViews\n          cachedRequests\n          cachedBytes\n          encryptedRequests\n          encryptedBytes\n          responseStatusMap {\n            edgeResponseStatus\n            requests\n            __typename\n          }\n          __typename\n        }\n        uniq {\n          uniques\n          __typename\n        }\n                __typename\n      }\n            __typename\n    }\n    __typename\n  }\n}\n"})')
+
+  errors=$(echo "$data" | jq -r '.errors')
+
+  if [ "$errors" != "null" ]; then
+    exit_with_error "error reading zone traffic: $(echo "$data" | jq -r '.errors')"
+  fi
+
+  bytes=$(echo "$data" | jq -r '.data.viewer.zones[].statsOverTime[].sum | "\(.bytes);\(.cachedBytes);\(.cachedRequests);\(.encryptedBytes);\(.encryptedRequests);\(.pageViews);\(.requests)"')
+  echo "$bytes"
+}
+
+# checking environment variables
+[ -z "$CLOUDFLARE_EMAIL" ] && exit_with_error "CLOUDFLARE_EMAIL is not set"
+[ -z "$CLOUDFLARE_API_KEY" ] && exit_with_error "CLOUDFLARE_API_KEY is not set"
+if [ "$#" -ne 2 ]; then
+  exit_with_error "Run script with date range arguments, for example: [2020-07-01] [2020-07-31] (data will include the respective days)"
+fi
+
+for date in "$@"
+do
+  date "+%Y-%m-%d" -d "$date" > /dev/null
+  if [ "$?" -ne 0 ]; then
+    exit_with_error "Dates must be passed in format Year-Month-Day"
+  fi
+done
+
+# Pulling all account tags that the user has access to
+accounts=$(get_accounts)
+
+echo "Account Name;Zone Name;Bytes;CachedBytes;CachedRequests;EncryptedBytes;EncryptedRequests;PageViews;Requests"
+while read -r account
+do
+  IFS='#' read -r -a accountInfo <<< "$account"
+
+  while read -r zone
+  do
+    IFS='#' read -r -a zoneInfo <<< "$zone"
+    echo "${accountInfo[1]};${zoneInfo[1]};$(get_traffic "${zoneInfo[0]}" "$1" "$2")"
+
+  done <<< "$(get_zones "${accountInfo[0]}")"
+done <<< "$accounts"
